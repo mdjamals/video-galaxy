b764d07f7b46b5c59687ee1a406d815bcfccb2d5
roshann29 <roshan.n29@gmail.com>
2020-09-06 13:30:13 +0530

    basic style check

--- src/components/app/header-element.js
+++ src/components/app/header-element.js
@@ -5,54 +5,59 @@ export default class HeaderElement extends LitElement {
 
     static get styles() {
         return css`
-    :host {
-        display: block;
-    }
+            :host {
+                display: block;
+            }
 
-    .sidenav {
-        height: 100%;
-        width: 0;
-        position: fixed;
-        z-index: 1;
-        top: 0;
-        left: 0;
-        background-color: #111;
-        overflow-x: hidden;
-        transition: 0.5s;
-        padding-top: 60px;
-    }
+            .sidenav {
+                height: 100%;
+                width: 0;
+                position: fixed;
+                z-index: 1;
+                top: 0;
+                left: 0;
+                background-color: #111;
+                overflow-x: hidden;
+                transition: 0.5s;
+                padding-top: 60px;
+            }
 
-    .sidenav a {
-        padding: 8px 8px 8px 32px;
-        text-decoration: none;
-        font-size: 25px;
-        color: #818181;
-        display: block;
-        transition: 0.3s;
-    }
+            .sidenav a {
+                padding: 8px 8px 8px 32px;
+                text-decoration: none;
+                font-size: 25px;
+                color: #818181;
+                display: block;
+                transition: 0.3s;
+            }
 
-    .sidenav a:hover {
-        color: #f1f1f1;
-    }
+            .sidenav a:hover {
+                color: #f1f1f1;
+            }
 
-    .sidenav .closebtn {
-        position: absolute;
-        top: 0;
-        right: 25px;
-        font-size: 36px;
-        margin-left: 50px;
-    }
+            .sidenav .closebtn {
+                position: absolute;
+                top: 0;
+                right: 25px;
+                font-size: 36px;
+                margin-left: 50px;
+            }
 
-    .menu {
-        font-size:30px;
-        cursor:pointer;
-    }
+            nav {
+                padding: 6px;
+                background-color: #474747;
+            }
 
-    @media screen and (max-height: 450px) {
-        .sidenav {padding-top: 15px;}
-        .sidenav a {font-size: 18px;}
-    }
-    `;
+            .menu {
+                font-size:30px;
+                cursor:pointer;
+            }
+
+            @media screen and (max-height: 450px) {
+                .sidenav {padding-top: 15px;}
+                .sidenav a {font-size: 18px;}
+            }
+        `;
     }
 
     openDrawer(e) {
@@ -66,15 +71,19 @@ export default class HeaderElement extends LitElement {
 
     render() {
         return html`
-        <div id="drawer" class="sidenav"  @click="${this.closeDrawer}">
-            <a href="javascript:void(0)" class="closebtn">&times;</a>
-            <nav-link href="/">Home</nav-link>
-            <nav-link href="/info">Info</nav-link>
-            <nav-link href="/info?data=12345" >Info?data=12345</nav-link>
-            <nav-link href="/user/14">user/14</nav-link>
-        </div>
-        
-        <span class="menu" @click="${this.openDrawer}">&#9776; open</span>
+            <div id="drawer" class="sidenav"  @click="${this.closeDrawer}">
+                <a href="javascript:void(0)" class="closebtn">&times;</a>
+                <nav-link href="/">Home</nav-link>
+                <nav-link href="/info">Info</nav-link>
+                <nav-link href="/info?data=12345" >Info?data=12345</nav-link>
+                <nav-link href="/user/14">user/14</nav-link>
+            </div>
+            
+            <nav class="">
+                <span class="menu" @click="${this.openDrawer}">&#9776; open</span>
+                <a class="navbar-brand" href="#">Navbar</a>
+            </nav>
+            
         `;
     }
 }
