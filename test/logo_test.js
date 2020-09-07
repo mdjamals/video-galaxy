b680ca45a3bd6e1e2d287853de13354e40c2f530
roshann29 <roshan.n29@gmail.com>
2020-09-07 22:31:45 +0530

    Test suite for logo

---
+++ test/logo_test.js
@@ -0,0 +1,20 @@
+import AppLogoElement from '../src/components/app/app-logo-element.js';
+import {fixture, html} from '@open-wc/testing';
+
+const assert = chai.assert;
+suite('Logo test', () => {
+  test('is defined', () => {
+    const el = document.createElement('app-logo-element');
+    assert.instanceOf(el, AppLogoElement);
+  });
+
+  test('renders with default values', async () => {
+    const el = await fixture(html`<app-logo-element></app-logo-element>`);
+    assert.shadowDom.equal(
+        el,
+        `<div class="triangle">
+            <div class="empty"></div>
+        </div>`
+    );
+    });
+});
